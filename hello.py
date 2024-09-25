80b303812cb1979ab82b4b3194e36acf79e85501
Fernando Dias <ferpaesdias@gmail.com>
2024-09-25 14:43:29 -0300

    Update

--- hello.py
+++ hello.py
@@ -10,26 +10,51 @@ Tenha a variável LANG devidamente configurada, ex:
 
     exporte LANG=pt_BR
 
+Ou informe através do CLI argument `--VAR_TEST`
+
+Ou o usuário terá que digitar.
+
 Execução:
 
     python3 hello.py
     ou
     ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Fernando Dias"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("VAR_TESTE", "pt_BR")[:5]
+arguments = {"VAR_TESTE": None, "count": 1}
+
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print("Invalid Option '{key}'")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["VAR_TESTE"]
+if current_language is None:
+    # TODO: User repetição
+    if "VAR_TESTE" in os.environ:
+        current_language = os.getenv("VAR_TESTE")
+    else:
+        current_language = input('Choose a language:')
+
+current_language = current_language[:5]
 
 msg = {
     "en_US":"Hello World",
     "pt_BR":"Olá, Mundo!!",
     "it_IT":"Ciao, Mondo",
-    "esp_SP":"Hola, Mundo!!",
+    "es_ES":"Hola, Mundo!!",
     "fr_FR":"Bonjour, Monde!!"
 }
 
-print(msg[current_language])  
+print(msg[current_language] * int(arguments["count"]))  
