9a06541bdc17f91fa5d790cd42e6ffdf0bd6375b
Fernando Dias <ferpaesdias@gmail.com>
2024-09-26 14:30:05 -0300

    Uodate

--- prefixcalc.py
+++ prefixcalc.py
@@ -33,7 +33,6 @@ import sys
 from datetime import datetime
 arguments = sys.argv[1:]
 
-# TODO: Exceptions
 if not arguments:
     operation =input("operacao:")
     n1 = input("n1:")
@@ -64,8 +63,12 @@ for num in nums:
         num = int(num)
     validated_nums.append(num)
 
-n1, n2 = validated_nums
-
+try:
+    n1, n2 = validated_nums
+except ValueError as e: 
+    print(str(e))
+    sys.exit(1)
+    
 # TODO: Usar dict de funções
 if operation == "sum":
     result = n1 + n2
@@ -81,7 +84,14 @@ filepath = os.path.join(path, "prefixcalc.log")
 timestamp = datetime.now().isoformat()
 user = os.getenv("USER", "anonimous")
 
-with open(filepath, "a") as file_:
-    file_.write(f"{timestamp} - {user} -  {operation},{n1},{n2} = {result}\n")
+print(f"O resultado é {result}") 
 
-print(f"O resultado é {result}")          
+try:
+    with open(filepath, "a") as file_:
+        file_.write(f"{timestamp} - {user} -  {operation},{n1},{n2} = {result}\n")
+except PermissionError as e:
+    # TODO: logging
+    print(str(e))
+    sys.exit(1)
+
+         
